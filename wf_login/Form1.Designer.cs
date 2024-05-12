d335b7e3608ccf44f3a8f150ca6e21af76fef889
Rodrigo Pinto <164094140+rodrigofpinto@users.noreply.github.com>
2024-05-13 00:45:42 +0100

    aa

--- wf_login/Form1.Designer.cs
+++ wf_login/Form1.Designer.cs
@@ -28,13 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.checkShowPass = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(70, 422);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(127, 64);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(261, 422);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(127, 64);
+            this.btnRegister.TabIndex = 0;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(70, 226);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(318, 30);
+            this.txtUser.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(70, 288);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(318, 30);
+            this.txtPass.TabIndex = 1;
+            // 
+            // checkShowPass
+            // 
+            this.checkShowPass.AutoSize = true;
+            this.checkShowPass.Location = new System.Drawing.Point(70, 343);
+            this.checkShowPass.Name = "checkShowPass";
+            this.checkShowPass.Size = new System.Drawing.Size(125, 20);
+            this.checkShowPass.TabIndex = 2;
+            this.checkShowPass.Text = "Show Password";
+            this.checkShowPass.UseVisualStyleBackColor = true;
+            this.checkShowPass.CheckedChanged += new System.EventHandler(this.checkShowPass_CheckedChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(272, 345);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(116, 16);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forgot Password?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(146, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 133);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(458, 552);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.checkShowPass);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.CheckBox checkShowPass;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- wf_login/Form1.cs
+++ wf_login/Form1.cs
@@ -15,11 +15,97 @@ namespace wf_login
 
     public partial class Form1 : Form
     {
-        SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=contactos;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-        SqlCommand cmd = new SqlCommand();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\pinto\\Documents\\wf_login\\wf_login\\Database1.mdf;Integrated Security=True";
+            string query = "SELECT COUNT(*) FROM dados WHERE username=@username AND password=@password";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@username", txtUser.Text);
+                    cmd.Parameters.AddWithValue("@password", txtPass.Text);
+                    int count = (int)cmd.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        Form2 f2 = new Form2();
+                        f2.Show();
+                        this.Hide();
+                        MessageBox.Show("Login com sucesso", "Info");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error login", "Erro");
+                    }
+                }
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text.Length < 4)
+            {
+                MessageBox.Show("O nome de usuário deve ter pelo menos 4 caracteres.", "Erro de Registro");
+                return;
+            }
+
+            if (txtPass.Text.Length < 8)
+            {
+                MessageBox.Show("A senha deve ter pelo menos 8 caracteres.", "Erro de Registro");
+                return;
+            }
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\pinto\\Documents\\wf_login\\wf_login\\Database1.mdf;Integrated Security=True";
+            string checkQuery = "SELECT COUNT(*) FROM dados WHERE username=@username";
+            string insertQuery = "INSERT INTO dados (username, password) VALUES (@username, @password)";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                // Verifica se o usuário já existe
+                using (SqlCommand cmdCheck = new SqlCommand(checkQuery, con))
+                {
+                    cmdCheck.Parameters.AddWithValue("@username", txtUser.Text);
+                    int userExists = (int)cmdCheck.ExecuteScalar();
+
+                    if (userExists > 0)
+                    {
+                        MessageBox.Show("Este nome de usuário já está sendo usado.", "Erro de Registro");
+                        return;
+                    }
+                }
+
+                // Insere o novo usuário
+                using (SqlCommand cmdInsert = new SqlCommand(insertQuery, con))
+                {
+                    cmdInsert.Parameters.AddWithValue("@username", txtUser.Text);
+                    cmdInsert.Parameters.AddWithValue("@password", txtPass.Text);
+                    int result = cmdInsert.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Registro realizado com sucesso!", "Registro Concluído");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao registrar usuário.", "Erro de Registro");
+                    }
+                }
+            }
+        }
+
+        private void checkShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPass.PasswordChar=checkShowPass.Checked?'\0' : '*';
         }
     }
 }
