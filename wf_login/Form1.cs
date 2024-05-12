081be7cefcf24cc6fa32f306d3717c554c43e2f2
Rodrigo Pinto <164094140+rodrigofpinto@users.noreply.github.com>
2024-05-12 23:54:57 +0100

    Add project files.

---
+++ wf_login/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace wf_login
+{
+
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=contactos;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        SqlCommand cmd = new SqlCommand();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
